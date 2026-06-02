f0235d1c58635212c13be350483784e1aae0016b
commit2main <229308316+commit2main@users.noreply.github.com>
2026-06-02 07:24:53 +0000

    bootstrap with next.js + shadcn/ui

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+
+export const metadata: Metadata = {
+  title: "HDB Trackr",
+  description:
+    "A web application for tracking HDB resale prices and analytics.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={cn("h-full", "antialiased", inter.variable)}>
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
