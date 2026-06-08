87fd430f020a8e27c276e312fd8277590ea94a72
Kitsunez <nyx2753@gmail.com>
2026-06-08 02:43:28 +0000

    added column names explicit so can be referenced in config

--- scripts/dataset_config/gyms.py
+++ scripts/dataset_config/gyms.py
@@ -4,12 +4,14 @@ from .base import DatasetConfig
 DATASET = DatasetConfig(
     key="gyms",
     dataset_id="d_b3ae090692ecf632116c9885cfbd3424",
-    column_names=[
-        "type",
-        "properties.Name",
-        "properties.Description",
-        "geometry.type",
-        "geometry.coordinates",
-    ],
+    column_names=[],
     filters=[],
 )
+"""
+Fields
+    type
+    properties.Name
+    properties.Description
+    geometry.type
+    geometry.coordinates
+"""
