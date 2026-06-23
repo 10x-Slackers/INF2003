880282de994167ee45a50ccf02b7eb31fa7ba001
Kitsunez <nyx2753@gmail.com>
2026-06-23 07:31:36 +0000

    combined database setup with transform

--- scripts/main.py
+++ scripts/main.py
@@ -1,25 +1,40 @@
 from scripts.db.mariadb import Database
-from scripts.db.mongodb import setup_mongodb
+from scripts.db.mongodb import setup_mongodb, MongoDB
 from urllib.parse import quote_plus
 import os
+from scripts.extract import DataGovDatasetClient
+import pandas as pd
+from scripts.dataset_config import DATASETS
+from scripts.dataset_transform import transform_datasets
 
 
 def main():
-    test_db_connection()
+    mongodb, mariadb = setup_db()
+    client = DataGovDatasetClient(
+        api_key="v2:9014eec42fec26e6355db0bf611d5ae58edad9ede6e68108ca59d190fa5ae2bf:QyLukoit5tWykUL0YvEvm8lnkQbeg2F2",
+    )
+    dataframes: dict[str, pd.DataFrame] = {}
+
+    for config in DATASETS:
+        dataframe = client.fetch_dataset(config)
+        dataframes[config.key] = dataframe
+    transformed_data = transform_datasets(dataframes)
+    # Loading script goes here
 
 
-def test_db_connection():
+def setup_db() -> tuple[MongoDB, Database]:
     try:
-        db = Database(
+        mariadb = Database(
             host=os.environ.get("DB_HOST", "mariadb"),
             user=os.environ.get("DB_USER", "root"),
             password=os.environ.get("DB_PASSWORD", "P@ssw0rd"),
             database=os.environ.get("DB_NAME", "inf2003"),
         )
         print("MariaDB connection successful.")
-        db.close()
+        mariadb.close()
     except Exception as e:
         print(f"An error occurred: {e}")
+        raise
     try:
         mongodb = setup_mongodb(
             uri=os.environ.get(
@@ -32,6 +47,8 @@ def test_db_connection():
         mongodb.close()
     except Exception as e:
         print(f"An error occurred while connecting to MongoDB: {e}")
+        raise
+    return mongodb, mariadb
 
 
 if __name__ == "__main__":
