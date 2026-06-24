adccc8ac6bd203bb9778a844a83a58b967c38a77
Kitsunez <nyx2753@gmail.com>
2026-06-24 07:18:59 +0000

    change auth to use db module created

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,5 @@
 import { NextAuthOptions } from "next-auth";
-import { pool } from "@/lib/db";
+import { pool } from "@/lib/db/mariadb";
 import Credentials from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 
--- lib/db.ts
+++
@@ -1,8 +0,0 @@
-import mysql from "mysql2/promise";
-
-export const pool = mysql.createPool({
-  host: process.env.DB_HOST || "mariadb",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "P@ssw0rd",
-  database: process.env.DB_NAME || "inf2003",
-});
