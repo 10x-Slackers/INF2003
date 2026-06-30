bf9ae89f99fad55e5810d7c965d7f4acb3d76629
Kitsunez <nyx2753@gmail.com>
2026-06-30 05:25:50 +0000

    add utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { MongoError } from "mongodb";
-import { QueryError } from "mysql2";
+import type { QueryError } from "mysql2";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
